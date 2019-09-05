fab2bed8266d784cd0e605dd0539590eb08fbb2f
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-06 09:34:24 +1200

    Create Homepage component and add content to it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
