6a345a56c0f5da0c57533f5fa7319402c1ad39d8
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-09 14:58:03 +1200

    Add AngularFire2 for Firabase authentication and add routing to the app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomepageComponent} from "./homepage/homepage.component";
+import {LoginComponent} from "./homepage/login/login.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/homepage', pathMatch: 'full'
+  },
+  {
+    path: 'homepage', component: HomepageComponent
+  }, {
+    path: 'login', component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+
+// use export routingComponents to prevent import every module in app.module.ts
+export const routingComponents = [HomepageComponent, LoginComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { FooterComponent } from './footer/footer.component';
+import {AngularFireModule} from "@angular/fire";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    FooterComponent
+    routingComponents,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
