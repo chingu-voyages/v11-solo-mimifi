66ae4e9c6945c653f3673e1eb7b8512834ac6390
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-12 14:33:50 +1200

    Remove afterClosed(), as it is not needed.

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -14,20 +14,14 @@ export class HomepageComponent {
   }
 
   openRegistration(): void {
-    const dialogRegistration = this.dialog.open(RegistrationComponent, {
+    this.dialog.open(RegistrationComponent, {
       width: '600px'
     });
-    dialogRegistration.afterClosed().subscribe(result => {
-      console.log(result.toString())
-    })
   }
 
   openSignIn() {
-    const dialogSignIn = this.dialog.open(SignInComponent, {
+    this.dialog.open(SignInComponent, {
       width: '600px'
     });
-    dialogSignIn.afterClosed().subscribe(result => {
-      console.log('Sign In dialog is closed.')
-    })
   }
 }
