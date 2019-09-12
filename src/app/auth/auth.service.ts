8304787f33ace1ef50b4160ca51c2d453865459f
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-12 14:31:21 +1200

    Delete return parameter in all methods, remove console.log and tidy up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,9 +10,9 @@ import {Router} from "@angular/router";
 export class AuthService {
   userData: User;
 
-
-  constructor(public afAuth: AngularFireAuth,
-              public router: Router) {
+  constructor(
+    public afAuth: AngularFireAuth,
+    public router: Router) {
 
     this.afAuth.authState.subscribe(user => {
       if (user) {
@@ -23,30 +23,28 @@ export class AuthService {
         localStorage.setItem('userData', null);
         JSON.parse(localStorage.getItem('userData'))
       }
-      console.log(user)
     })
   }
 
   signUp(email, password) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
       .then((result) => {
         this.sendVerificationEmail();
         window.alert('You have been successfully registered!');
-        console.log(result.user)
       }).catch((error) => {
         window.alert(error.message)
       })
   }
 
   sendVerificationEmail() {
-    return this.afAuth.auth.currentUser.sendEmailVerification()
+    this.afAuth.auth.currentUser.sendEmailVerification()
       .then(() => {
         this.router.navigate(['/homepage'])
       })
   }
 
   signIn(email, password) {
-    return this.afAuth.auth.signInWithEmailAndPassword(email, password)
+    this.afAuth.auth.signInWithEmailAndPassword(email, password)
       .then((result) => {
         this.router.navigate(['/homepage']);
         console.log('User is signed in successfully.')
