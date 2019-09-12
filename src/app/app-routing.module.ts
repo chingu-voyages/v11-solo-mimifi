50a76ccb95138cd958dd173eef2b601159a6daee
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-12 16:46:10 +1200

    Tidy up the app component and create unit test for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
+import {FooterComponent} from "./footer/footer.component";
 import {RegistrationComponent} from "./homepage/registration/registration.component";
 import {SignInComponent} from "./homepage/sign-in/sign-in.component";
 
@@ -25,4 +26,4 @@ export class AppRoutingModule {
 }
 
 // use export routingComponents to prevent import every module in app.module.ts
-export const routingComponents = [HomepageComponent, RegistrationComponent, SignInComponent];
+export const routingComponents = [HomepageComponent, FooterComponent , RegistrationComponent, SignInComponent];
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {AppComponent} from './app.component';
+import {FooterComponent} from "./footer/footer.component";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +10,7 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, FooterComponent
       ],
     }).compileComponents();
   }));
@@ -18,18 +19,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'todo-web-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-web-app');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-web-app!');
   });
 });
