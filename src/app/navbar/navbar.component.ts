f494ede7338715a2e05cefa76f065d2db969965a
Mantab Tadayon <mt.tadayon@gmail.com>
2019-09-13 17:29:03 +1200

    Add navbar and complete style and content

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {RegistrationComponent} from "../homepage/registration/registration.component";
+
+export enum PageName {
+  DASHBOARD = 'dashboard',
+  TASKLIST = 'tasklist'
+}
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  @Input() public pageName: PageName;
+
+  constructor(public dialog: MatDialog) {
+  }
+
+  openAddTrip() {
+    this.dialog.open(RegistrationComponent, {
+      width: '600px'
+    })
+  }
+}
